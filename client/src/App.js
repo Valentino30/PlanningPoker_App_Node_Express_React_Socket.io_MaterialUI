d27fcc5a5bc1c8700fd3010635833315dff1d17c
Valentino <valentino@secret-source.eu>
2022-04-15 13:16:33 +0200

    Remove CRA clutter and add new favicon

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import {
-  BrowserRouter as Router,
-  Switch,
   Route,
+  Switch,
   Redirect,
+  BrowserRouter as Router,
 } from "react-router-dom";
 
 import "./App.css";
